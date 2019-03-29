bb068f30c1856f9bc4df4ae29a8e431ee5f26696
liaoze <616655260@qq.com>
2019-03-29 18:33:34 +0800

    testc

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  itestC
+//
+//  Created by 廖泽 on 2019/3/18.
+//  Copyright © 2019年 廖泽. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
